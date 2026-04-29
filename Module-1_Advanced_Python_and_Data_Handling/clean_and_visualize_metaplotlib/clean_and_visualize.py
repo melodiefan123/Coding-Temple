6e8c132643e968615df23ede9fb08c8f73b502c6
Sav-Test <melodie.fan@gmail.com>
2026-04-29 01:19:36 -0700

    plotting

--- Module-1_Advanced_Python_and_Data_Handling/clean_and_visualize_metaplotlib/clean_and_visualize.py
+++ Module-1_Advanced_Python_and_Data_Handling/clean_and_visualize_metaplotlib/clean_and_visualize.py
@@ -12,7 +12,45 @@ messy = pd.DataFrame({
                "West", "east", "North", "South", "West"],
 })
 
+
 # Clean it: Standardize names/regions, convert types, handle missing/invalid values, remove duplicates
+messy = messy.dropna(subset=['product'])
+messy['product'] = messy['product'].str.strip().str.title()
+messy['region'] = messy['region'].str.strip().str.title()
+messy['sales'] = pd.to_numeric(messy['sales'], errors = "coerce")
+messy = messy.dropna(subset=["sales"])
+messy = messy[messy["sales"] >0]
+messy['date'] = pd.to_datetime(messy['date'], errors="coerce")
 
 # Analyze and visualize: Total sales by product (bar), daily trend (line), distribution (histogram)
 # Save visualizations to a PNG file
+sales_by_product = messy.groupby("product")["sales"].sum()
+plt.bar(sales_by_product.index, sales_by_product.values)
+plt.title("Total Sales By Product") 
+plt.xlabel("Products")
+plt.ylabel("Sales")
+plt.savefig("Sales_By_Product_Bar.png", dpi=150)
+plt.show()
+
+#Line Chart
+daily_trend = messy.groupby("date")["sales"].sum()
+plt.plot(daily_trend.index, daily_trend.values)
+plt.title("Daily Trend")
+plt.xlabel("Date")
+plt.ylabel("Sales")
+plt.savefig("Daily_Trend_Line.png", dpi=150)
+plt.show()
+
+
+#Histogram
+plt.hist(messy["sales"])
+plt.title("Distribution")
+plt.xlabel("Sales")
+plt.ylabel("Frequency")
+plt.savefig("Distribution_Histogram.png", dpi=150)
+plt.show()
+
+
+
+
+
