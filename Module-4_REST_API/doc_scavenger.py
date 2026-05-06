9f6464ce229303394f16b4ba330387cc3df6e9f8
Sav-Test <melodie.fan@gmail.com>
2026-05-06 13:58:46 -0700

    doc scavenger update

--- Module-4_REST_API/doc_scavenger.py
+++ Module-4_REST_API/doc_scavenger.py
@@ -3,13 +3,13 @@
 # Prints the remaining rate limit after the request
 
 import requests
-responses = requests.get("https://api.github.com/search/repositories", params={"q": "user:google", "sort": "stars","order": "desc", "per_page": 3}, headers={"Accept": "application/vnd.github+json"})
+responses = requests.get("https://api.github.com/users/google/repos", params={"sort": "stars","order": "desc", "per_page": 3}, headers={"Accept": "application/vnd.github+json"})
 data = responses.json()
-for repo in data['items']:
+for repo in data:
     print(f"Name: {repo['name']}")
-    print(f"Description: {repo['description']}")
+    print(f"Description: {repo['description'] or "No description provided."}")
     print(f"Stars: {repo['stargazers_count']}")
-    print(f"Language: {repo['language']}")
+    print(f"Language: {repo['language'] or "Not specified"}")
     print("-" * 40)
 
 print(f"Remaining rate limit: {responses.headers.get('X-RateLimit-Remaining')}")
