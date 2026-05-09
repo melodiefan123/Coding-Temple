24da86c8cd319f94977514dc14086767d348b502
Sav-Test <melodie.fan@gmail.com>
2026-05-09 11:25:01 -0700

    main.py fix for all

--- Module-5-FastAPI_Development/library_search_fastapi/main.py
+++ Module-5-FastAPI_Development/library_search_fastapi/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from app.config import settings
-from app.routers import ingredients, recipes
-
+from app.routers import books
 app = FastAPI(
     title=settings.app_name, 
     description="A simple API for library search functionality", 
     version="1.0.0")
 
-app.include_router(recipes.router)
-app.include_router(ingredients.router)
+app.include_router(books.router)
 
 @app.get("/")
 def root(): 
