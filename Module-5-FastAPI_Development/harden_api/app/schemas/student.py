93aa8508bd31671f1d68036b05c9ee9f334a1ad5
Sav-Test <melodie.fan@gmail.com>
2026-05-11 13:37:27 -0700

    harden api

---
+++ Module-5-FastAPI_Development/harden_api/app/schemas/student.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+
+class StudentCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=200, description="name of the student")
+    email: str = Field(min_length=1, max_length=100,description="The email of the student")
+    grade_level: Optional[int] = Field(None, ge=1, le=12, description="The grade level of the student")
+    gpa: float = Field(0.0, ge=0.0, le=4.0, description="The GPA of the student")
+    is_enrolled: bool = Field(True, description="Whether the student is currently enrolled")
+
+class StudentUpdate(BaseModel):
+    name: str = Field(min_length=1, max_length=200, description="name of the student")
+    email: str = Field(min_length=1, max_length=100,description="The email of the student")
+    grade_level: int = Field(ge=1, le=12, description="The grade level of the student")
+    gpa: Optional[float] = Field(None, ge=0.0, le=4.0, description="The GPA of the student")
+    is_enrolled: bool = Field(True, description="Whether the student is currently enrolled")
+
+class StudentPatch(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=200, description="name of the student")
+    email: Optional[str] = Field(None, min_length=1, max_length=100,description="The email of the student")
+    grade_level: Optional[int] = Field(None, ge=1, le=12, description="The grade level of the student")
+    gpa: Optional[float] = Field(None, ge=0.0, le=4.0, description="The GPA of the student")
+    is_enrolled: Optional[bool] = Field(None, description="Whether the student is currently enrolled")
+
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    grade_level: Optional[int]
+    gpa: Optional[float]
+    is_enrolled: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
